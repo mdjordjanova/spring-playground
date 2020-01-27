28dbeaf8ff728db46e4ec4cd55e00ca04d9e722e
maria <mariadjordjanova@marias-mbp.home>
2020-01-27 20:01:31 +0100

    add rest controllers

--- spring-boot-redis/src/main/java/com/demo/controller/BookController.java
+++ spring-boot-redis/src/main/java/com/demo/controller/BookController.java
@@ -1,7 +1,32 @@
 package com.demo.controller;
 
+import com.demo.model.Book;
+import com.demo.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @PutMapping(value = "/books")
+    public ResponseEntity<Book> add(@RequestBody Book book) {
+        bookService.save(book);
+        return new ResponseEntity<>(book, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/books")
+    public ResponseEntity<List<Book>> getAll() {
+        List<Book> booksList = bookService.findAll();
+        return new ResponseEntity<>(booksList, HttpStatus.OK);
+    }
 }
--- spring-boot-redis/src/main/java/com/demo/service/AuthorService.java
+++ spring-boot-redis/src/main/java/com/demo/service/AuthorService.java
@@ -1,13 +1,14 @@
 package com.demo.service;
 
 import com.demo.model.Author;
-import com.demo.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -24,6 +25,11 @@ public class AuthorService {
     public void save(final Author author) {
         author.setId(UUID.randomUUID().toString());
         authorRedisTemplate.opsForValue().set(getRedisKey(author.getId()), author);
+    }
+
+    @Cacheable
+    public List<Author> findAll() {
+        return authorRedisTemplate.opsForValue().multiGet(authorRedisTemplate.keys(getRedisKey("*")));
     }
 
     private String getRedisKey(final String key) {
